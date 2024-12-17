aa2862776e620a7fbcb6ec9604d98f193b5e111c
rbrin <rbrinson2@gmail.com>
2024-12-17 08:44:47 -0500

    reshuffled the design files

--- orfs_test.sh
+++ orfs_test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 docker run --rm -it \
-           -v $(pwd)/:/OpenROAD-flow-scripts/flow/designs/src/verilator_example \
-           -v $(pwd)/:/OpenROAD-flow-scripts/flow/sky130hd/verilator_example \
+           -v $(pwd)/design:/OpenROAD-flow-scripts/flow/designs/src/orfs_test \
            -e DISPLAY=${DISPLAY} \
            -v /tmp/.X11-unix:/tmp/.X11-unix \
            -v ${HOME}/.Xauthority:/.Xauthority \
